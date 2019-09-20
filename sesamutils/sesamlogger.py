19c26017f0d8808a7563dc626f9fee0ab53be269
Ravish Ranjan <ravish.ranjan@sesam.io>
2019-09-20 14:28:06 +0200

    Update sesamlogger.py
    Added  the feature to log request URls if you want to.

--- sesamutils/sesamlogger.py
+++ sesamutils/sesamlogger.py
@@ -1,10 +1,11 @@
 import logging
 import os
+import paste.translogger
 
 
-def sesam_logger(logger_name, timestamp=False):
+def sesam_logger(logger_name, app=None, timestamp=False):
     if logger_name is None or logger_name is '':
-        raise ValueError('Please provide the valid logger name.')
+        raise ValueError('Please provide a valid logger name.')
     logger = logging.getLogger(logger_name)
     log_level = os.getenv('LOG_LEVEL')
     level = logging.getLevelName(log_level.upper()) if log_level is not None else None
@@ -12,6 +13,11 @@ def sesam_logger(logger_name, timestamp=False):
     stdout_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'
                                                   if timestamp else '%(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(stdout_handler)
+    # Comment these two lines if you don't want access request logging
+    if app:
+        app.wsgi_app = paste.translogger.TransLogger(app.wsgi_app, logger_name=logger.name, setup_console_handler=False)
+        app.logger.addHandler(stdout_handler)
+        logger.propagate = False
     if not isinstance(level, int):
         logger.warning("Unsupported value or no LOG_LEVEL provided. Hence, setting default log level to INFO.")
         level = logging.INFO
